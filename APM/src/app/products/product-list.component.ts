d3f8bfdc7a6938286dc1d6ccf95b0d5f0b833c73
conklind <chad.conklin@das.cpa.com>
2020-10-08 20:04:48 -0700

    updates 10/08/2020

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
-//import { IProduct } from './product';
+import { IProduct } from './product';
 //import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
-  //styleUrls: ['./product-list.component.css']
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent /*implements OnInit*/ {
+export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   imageWidth = 50;
   imageMargin = 2;
   showImage = false;
-  listFilter = 'cart';
-  products: any[] =
+  filteredProducts: IProduct[];
+  products: IProduct[] =
   [
     {
       "productId": 1,
@@ -71,35 +71,38 @@ export class ProductListComponent /*implements OnInit*/ {
 
   // errorMessage = '';
 
-  // _listFilter = '';
-  // get listFilter(): string {
-  //   return this._listFilter;
-  // }
-  // set listFilter(value: string) {
-  //   this._listFilter = value;
-  //   this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
-  // }
+  _listFilter = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
 
   // filteredProducts: IProduct[] = [];
   // products: IProduct[] = [];
 
-  // //constructor(private productService: ProductService) { }
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
 
   // onRatingClicked(message: string): void {
   //   this.pageTitle = 'Product List: ' + message;
   // }
 
-  // performFilter(filterBy: string): IProduct[] {
-  //   filterBy = filterBy.toLocaleLowerCase();
-  //   return this.products.filter((product: IProduct) =>
-  //     product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
-  // }
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
 
-  // ngOnInit(): void {
+   ngOnInit(): void {
   //   this.productService.getProducts().subscribe({
   //     next: products => {
   //       this.products = products;
@@ -107,5 +110,5 @@ export class ProductListComponent /*implements OnInit*/ {
   //     },
   //     error: err => this.errorMessage = err
   //   });
-  // }
+   }
 }
