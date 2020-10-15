e3152d0733782991a1504a5ee1ce2dc0c9256970
chadc <chadconklin@outlook.com>
2020-10-14 19:32:02 -0700

    updates 10-14-2020

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -8,10 +10,31 @@ import { IProduct } from './product';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
-  product: IProduct;
-  constructor() { }
+  errorMessage = '';
+  product: IProduct | undefined;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private productService: ProductService) {
+   }
 
   ngOnInit(): void {
+    const param = this.route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
+    }
+  }
+
+  getProduct(id: number) {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
